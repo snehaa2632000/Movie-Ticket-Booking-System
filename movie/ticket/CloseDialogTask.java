f28771716cf7cfec04d292a11e82759ddd29dc7b
snehaa2632000 <55842333+snehaa2632000@users.noreply.github.com>
2020-04-24 12:56:44 +0530

    Add files via upload

---
+++ movie/ticket/CloseDialogTask.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movie.ticket;
+import java.util.TimerTask;
+
+/**
+ *
+ * @author Koushi
+ */
+public class CloseDialogTask extends TimerTask{
+    
+  public void run() {
+      System.exit(0);
+    //close dialog code goes here
+  }
+}
+    
+
